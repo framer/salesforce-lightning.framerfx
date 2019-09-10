323fbeb53ff87eddca264b43494ecce72e8f2cc6
Addison <addison@framer.com>
2019-09-10 14:32:30 +0200

    Start Data Table

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Todo:
 - Does card.tsx make sense? Look into attaching elements as children
 - How can I make carousel.tsx more user friendly instead of hardcoding the items?
 - How can I make combobox.tsx more user friendly
+- How can I make datatable.tsx more user friendly
--- code/datatable.tsx
+++ code/datatable.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as System from "@salesforce/design-system-react";
-import { ControlType,  addPropertyControls } from "framer";
+import { ControlType, addPropertyControls } from "framer";
 import { withHOC } from "./withHOC";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 
@@ -10,7 +10,7 @@ const style: React.CSSProperties = {
 };
 
 const InnerData: React.SFC = props => {
-  return <System.Data {...props} style={style} />;
+  return <System.DataTable {...props} style={style} />;
 };
 
 export const Data = withHOC(InnerData);
