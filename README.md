ef090b5a8a27c3558032a7666893d0010efcdba6
Addison <addison@framer.com>
2019-09-10 13:39:06 +0200

    Start carousel, see readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Salesforce Lightning Design System
+
+Todo:
+
+- Does app.tsx belong?
+- Does builder.tsx belong?
+
+- Does card.tsx make sense? Look into attaching elements as children
+- How can I make carousel.tsx more user friendly instead of hardcoding the items?
--- code/carousel.tsx
+++ code/carousel.tsx
@@ -1,45 +1,97 @@
 import * as React from "react";
 import * as System from "@salesforce/design-system-react";
-import { ControlType,  addPropertyControls } from "framer";
+import { ControlType, addPropertyControls } from "framer";
 import { withHOC } from "./withHOC";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
+const InnerCarousel = props => {
+  const items = [
+    {
+      buttonLabel: "Get Started",
+      id: 1,
+      heading: "Visit App Exchange",
+      description: "Extend Salesforce with the #1 business marketplace.",
+      imageAssistiveText: "Appy",
+      src: "/assets/images/carousel/carousel-01.jpg",
+      href: "https://www.salesforce.com"
+    },
+    {
+      buttonLabel: "Get Started",
+      id: 2,
+      heading: "Click to Customize",
+      description:
+        "Use the Object Manager to add fields, build layouts, and more.",
+      imageAssistiveText: "Apps",
+      src: "/assets/images/carousel/carousel-02.jpg",
+      href: "https://www.salesforce.com"
+    },
+    {
+      buttonLabel: "Get Started",
+      id: 3,
+      heading: "Download Salesforce Apps",
+      description: "Get the mobile app that's just for Salesforce admins.",
+      imageAssistiveText: "Salesforce Apps",
+      src: "/assets/images/carousel/carousel-03.jpg",
+      href: "https://www.salesforce.com"
+    },
+    {
+      buttonLabel: "Get Started",
+      id: 4,
+      heading: "Carousel Item 4",
+      description: "Description for carousel item #4",
+      imageAssistiveText: "Apps",
+      src: "/assets/images/carousel/carousel-02.jpg",
+      href: "https://www.salesforce.com"
+    },
+    {
+      buttonLabel: "Learn More",
+      id: 5,
+      heading: "Carousel Item 5",
+      description: "Description for carousel item #5",
+      imageAssistiveText: "Appy",
+      src: "/assets/images/carousel/carousel-01.jpg",
+      href: "https://www.salesforce.com"
+    },
+    {
+      buttonLabel: "Learn More",
+      id: 6,
+      heading: "Carousel Item 6",
+      description: "Description for carousel item #6",
+      imageAssistiveText: "Salesforce Apps",
+      src: "/assets/images/carousel/carousel-03.jpg",
+      href: "https://www.salesforce.com"
+    },
+    {
+      buttonLabel: "Learn More",
+      id: 7,
+      heading: "Carousel Item 7",
+      description: "Description for carousel item #7",
+      imageAssistiveText: "Apps",
+      src: "/assets/images/carousel/carousel-02.jpg",
+      href: "https://www.salesforce.com"
+    }
+  ];
 
-const InnerCarousel: React.SFC = props => {
-  return <System.Carousel {...props} style={style} />;
+  return <System.Carousel {...props} items={items} />;
 };
 
 export const Carousel = withHOC(InnerCarousel);
 
 Carousel.defaultProps = {
-  width: 150,
-  height: 50
+  width: 410,
+  height: 320
 };
 
 addPropertyControls(Carousel, {
-  assistiveText: {
-    title: "AssistiveText",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
   autoplayInterval: {
     title: "AutoplayInterval",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  className: {
-    title: "ClassName",
-    defaultValue: false,
-    type: ControlType.Boolean
+    defaultValue: 4000,
+    type: ControlType.Number
   },
   currentPanel: {
     title: "CurrentPanel",
-    defaultValue: false,
-    type: ControlType.Boolean
+    defaultValue: 0,
+    type: ControlType.Number
   },
   hasAutoplay: {
     title: "HasAutoplay",
@@ -51,7 +103,6 @@ addPropertyControls(Carousel, {
     defaultValue: false,
     type: ControlType.Boolean
   },
-  id: { title: "Id", defaultValue: false, type: ControlType.Boolean },
   isAutoplayOn: {
     title: "IsAutoplayOn",
     defaultValue: false,
@@ -62,30 +113,9 @@ addPropertyControls(Carousel, {
     defaultValue: false,
     type: ControlType.Boolean
   },
-  items: { title: "Items", defaultValue: false, type: ControlType.Boolean },
   itemsPerPanel: {
     title: "ItemsPerPanel",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onRenderItem: {
-    title: "OnRenderItem",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onRequestAutoplayToggle: {
-    title: "OnRequestAutoplayToggle",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onRequestPanelChange: {
-    title: "OnRequestPanelChange",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onItemClick: {
-    title: "OnItemClick",
-    defaultValue: false,
-    type: ControlType.Boolean
+    defaultValue: 3,
+    type: ControlType.Number
   }
 });
