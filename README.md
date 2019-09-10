35e21e3605a8e518b72e0e468502cd6b9f84add4
Addison <addison@framer.com>
2019-09-10 16:20:25 +0200

    started Popover

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Todo:
 - Add icon support for input
 - Can I override the default props for Icons if I import them? I.e files.tsx to have a different default icon
 - Need to fix sizing of Icon in Pill component
+- Need to fix and update Popover to be more robust and easier to use
 
 Notes:
 
--- code/popover.tsx
+++ code/popover.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as System from "@salesforce/design-system-react";
-import { ControlType,  addPropertyControls } from "framer";
+import { ControlType, addPropertyControls } from "framer";
 import { withHOC } from "./withHOC";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 
@@ -9,61 +9,50 @@ const style: React.CSSProperties = {
   height: "100%"
 };
 
-const InnerPopover: React.SFC = props => {
-  return <System.Popover {...props} style={style} />;
+const InnerPopover = props => {
+  return (
+    <System.Popover
+      body={props.body}
+      heading={props.heading}
+      disabled={props.disabled}
+      {...props}
+    >
+      <System.Button label="Trigger Popover" />
+    </System.Popover>
+  );
 };
 
 export const Popover = withHOC(InnerPopover);
 
 Popover.defaultProps = {
-  width: 150,
-  height: 50
+  width: 135,
+  height: 25
 };
 
 addPropertyControls(Popover, {
-  align: { title: "Align", defaultValue: false, type: ControlType.Boolean },
-  assistiveText: {
-    title: "AssistiveText",
-    defaultValue: false,
-    type: ControlType.Boolean
+  body: {
+    title: "Body",
+    defaultValue:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    type: ControlType.String
   },
-  ariaLabelledby: {
-    title: "AriaLabelledby",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  children: {
-    title: "Children",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  body: { title: "Body", defaultValue: false, type: ControlType.Boolean },
-  classNameBody: {
-    title: "ClassNameBody",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  classNameFooter: {
-    title: "ClassNameFooter",
-    defaultValue: false,
-    type: ControlType.Boolean
+  heading: {
+    title: "Heading",
+    defaultValue: "Header Title",
+    type: ControlType.String
   },
   disabled: {
     title: "Disabled",
     defaultValue: false,
     type: ControlType.Boolean
   },
-  footer: { title: "Footer", defaultValue: false, type: ControlType.Boolean },
-  footerStyle: {
-    title: "FooterStyle",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  footerWalkthroughActions: {
-    title: "FooterWalkthroughActions",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
+  // footer: { title: "Footer", defaultValue: false, type: ControlType.Boolean },
+
+  // footerWalkthroughActions: {
+  //   title: "FooterWalkthroughActions",
+  //   defaultValue: false,
+  //   type: ControlType.Boolean
+  // },
   hasNoCloseButton: {
     title: "HasNoCloseButton",
     defaultValue: false,
@@ -74,64 +63,23 @@ addPropertyControls(Popover, {
     defaultValue: false,
     type: ControlType.Boolean
   },
-  hasStaticAlignment: {
-    title: "HasStaticAlignment",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  hasNoTriggerStyles: {
-    title: "HasNoTriggerStyles",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  heading: { title: "Heading", defaultValue: false, type: ControlType.Boolean },
-  id: { title: "Id", defaultValue: false, type: ControlType.Boolean },
   isOpen: { title: "IsOpen", defaultValue: false, type: ControlType.Boolean },
-  onClick: { title: "OnClick", defaultValue: false, type: ControlType.Boolean },
-  onClose: { title: "OnClose", defaultValue: false, type: ControlType.Boolean },
-  onKeyDown: {
-    title: "OnKeyDown",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onMouseDown: {
-    title: "OnMouseDown",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onOpen: { title: "OnOpen", defaultValue: false, type: ControlType.Boolean },
-  onRequestClose: {
-    title: "OnRequestClose",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  onRequestTargetElement: {
-    title: "OnRequestTargetElement",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  position: {
-    title: "Position",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
   stepText: {
     title: "StepText",
-    defaultValue: false,
-    type: ControlType.Boolean
+    defaultValue: "This is some step text",
+    type: ControlType.String
   },
-  style: { title: "Style", defaultValue: false, type: ControlType.Boolean },
-  overlay: { title: "Overlay", defaultValue: false, type: ControlType.Boolean },
-  triggerClassName: {
-    title: "TriggerClassName",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  variant: { title: "Variant", defaultValue: false, type: ControlType.Boolean },
-  hoverCloseDelay: {
-    title: "HoverCloseDelay",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  openOn: { title: "OpenOn", defaultValue: false, type: ControlType.Boolean }
+  variant: {
+    title: "Variant",
+    defaultValue: "base",
+    type: ControlType.Enum,
+    options: ["base", "error", "walkthrough", "walkthrough-action", "warning"],
+    optionTitles: [
+      "base",
+      "error",
+      "walkthrough",
+      "walkthrough-action",
+      "warning"
+    ]
+  }
 });
