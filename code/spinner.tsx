cc5189d3c9d0a7e4ca881cd3011da2438b5dce0e
Addison <addison@framer.com>
2019-09-10 17:27:28 +0200

    Finish Spinner

--- code/spinner.tsx
+++ code/spinner.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as System from "@salesforce/design-system-react";
-import { ControlType,  addPropertyControls } from "framer";
+import { ControlType, addPropertyControls } from "framer";
 import { withHOC } from "./withHOC";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 
@@ -16,33 +16,28 @@ const InnerSpinner: React.SFC = props => {
 export const Spinner = withHOC(InnerSpinner);
 
 Spinner.defaultProps = {
-  width: 150,
+  width: 50,
   height: 50
 };
 
 addPropertyControls(Spinner, {
-  assistiveText: {
-    title: "AssistiveText",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  containerClassName: {
-    title: "ContainerClassName",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  containerStyle: {
-    title: "ContainerStyle",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  id: { title: "Id", defaultValue: false, type: ControlType.Boolean },
   isDelayed: {
     title: "IsDelayed",
     defaultValue: false,
     type: ControlType.Boolean
   },
-  isInput: { title: "IsInput", defaultValue: false, type: ControlType.Boolean },
-  size: { title: "Size", defaultValue: false, type: ControlType.Boolean },
-  variant: { title: "Variant", defaultValue: false, type: ControlType.Boolean }
+  size: {
+    title: "Size",
+    defaultValue: "medium",
+    type: ControlType.Enum,
+    options: ["x-small", "small", "medium", "large"],
+    optionTitles: ["x Small", "Small", "Medium", "Large"]
+  },
+  variant: {
+    title: "Variant",
+    defaultValue: "base",
+    type: ControlType.Enum,
+    options: ["base", "brand", "inverse"],
+    optionTitles: ["Base", "Brand", "Inverse"]
+  }
 });
