13293e98fa84f868d26f88882bc87769c8511077
Addison <addison@framer.com>
2019-09-10 12:09:01 +0200

    Remove extra imports

--- code/accordion.tsx
+++ code/accordion.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as System from "@salesforce/design-system-react";
-import { ControlType, PropertyControls, addPropertyControls } from "framer";
+import { ControlType, addPropertyControls } from "framer";
 import { withHOC } from "./withHOC";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 
--- code/withHOC.tsx
+++ code/withHOC.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as System from "@salesforce/design-system-react";
 
 import { url } from "framer/resource";
-
 const path = url("./code/icons");
 
 export function withHOC(Component): React.SFC {
