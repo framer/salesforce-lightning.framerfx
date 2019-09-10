f6b67517ec8993640d86f56252d8966e26bba9aa
Addison <addison@framer.com>
2019-09-10 12:53:38 +0200

    Finish Brand

--- code/brand.tsx
+++ code/brand.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as System from "@salesforce/design-system-react";
-import { ControlType,  addPropertyControls } from "framer";
+import { ControlType, addPropertyControls } from "framer";
 import { withHOC } from "./withHOC";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 
@@ -10,40 +10,36 @@ const style: React.CSSProperties = {
 };
 
 const InnerBrand: React.SFC = props => {
-  return <System.Brand {...props} style={style} />;
+  return <System.BrandBand {...props} style={style} />;
 };
 
 export const Brand = withHOC(InnerBrand);
 
 Brand.defaultProps = {
-  width: 150,
-  height: 50
+  width: 1152,
+  height: 500
 };
 
 addPropertyControls(Brand, {
-  children: {
-    title: "Children",
-    defaultValue: false,
-    type: ControlType.Boolean
+  image: {
+    title: "Image",
+    defaultValue: "default",
+    type: ControlType.Enum,
+    options: ["none", "default"],
+    optionTitles: ["None", "Default"]
   },
-  className: {
-    title: "ClassName",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  id: { title: "Id", defaultValue: false, type: ControlType.Boolean },
-  image: { title: "Image", defaultValue: false, type: ControlType.Boolean },
   backgroundSize: {
-    title: "BackgroundSize",
-    defaultValue: false,
-    type: ControlType.Boolean
+    title: "Background Size",
+    defaultValue: "contain",
+    type: ControlType.Enum,
+    options: ["contain", "cover"],
+    optionTitles: ["Contain", "Cover"]
   },
-  size: { title: "Size", defaultValue: false, type: ControlType.Boolean },
-  style: { title: "Style", defaultValue: false, type: ControlType.Boolean },
-  styleContainer: {
-    title: "StyleContainer",
-    defaultValue: false,
-    type: ControlType.Boolean
-  },
-  theme: { title: "Theme", defaultValue: false, type: ControlType.Boolean }
+  size: {
+    type: ControlType.Enum,
+    title: "Size",
+    options: ["small", "medium", "large"],
+    optionTitles: ["Small", "Medium", "Large"],
+    defaultValue: "medium"
+  }
 });
