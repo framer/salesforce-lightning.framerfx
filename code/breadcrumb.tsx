8db11b6d4e469b83791acb42249e38982b0998b6
Addison <addison@framer.com>
2019-09-10 13:05:46 +0200

    Finish Breadcrumb

--- code/breadcrumb.tsx
+++ code/breadcrumb.tsx
@@ -9,15 +9,43 @@ const style: React.CSSProperties = {
   height: "100%"
 };
 
-const InnerBreadcrumb: React.SFC = props => {
-  return <System.Breadcrumb {...props} style={style} />;
+const InnerBreadcrumb = props => {
+  let trail = props.trail.map((item, index) => {
+    return (
+      <a id={index} href="javascript:void(0);">
+        {item}
+      </a>
+    );
+  });
+
+  let Dropdown = (
+    <System.Dropdown
+      assistiveText={{ icon: "More Options" }}
+      iconCategory="utility"
+      iconName="down"
+      iconVariant="border-filled"
+      onSelect={() => {}}
+      options={props.dropdownTrail.map((item, index) => {
+        return { label: item, value: index };
+      })}
+    />
+  );
+
+  return (
+    <System.BreadCrumb
+      {...props}
+      style={style}
+      trail={trail}
+      overflowDropdownMenu={props.overflowDropdownMenu ? Dropdown : null}
+    />
+  );
 };
 
 export const Breadcrumb = withHOC(InnerBreadcrumb);
 
 Breadcrumb.defaultProps = {
-  width: 150,
-  height: 50
+  width: 300,
+  height: 20
 };
 
 addPropertyControls(Breadcrumb, {
@@ -26,16 +54,28 @@ addPropertyControls(Breadcrumb, {
     defaultValue: false,
     type: ControlType.Boolean
   },
-  id: { title: "Id", defaultValue: false, type: ControlType.Boolean },
   overflowDropdownMenu: {
     title: "OverflowDropdownMenu",
     defaultValue: false,
     type: ControlType.Boolean
   },
-  styleContainer: {
-    title: "StyleContainer",
-    defaultValue: false,
-    type: ControlType.Boolean
+  dropdownTrail: {
+    type: ControlType.Array,
+    title: "Dropdown Trail",
+    propertyControl: {
+      type: ControlType.String
+    },
+    defaultValue: ["Item 1", "Item 2"],
+    hidden(props) {
+      return props.overflowDropdownMenu === false;
+    }
   },
-  trail: { title: "Trail", defaultValue: false, type: ControlType.Boolean }
+  trail: {
+    type: ControlType.Array,
+    title: "Trail",
+    propertyControl: {
+      type: ControlType.String
+    },
+    defaultValue: ["Parent Entity", "Parent Record Name"]
+  }
 });
