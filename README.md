237e996f21a6e23b8a83e299e0d2ad1e655b542f
Addison <addison@framer.com>
2019-09-18 15:57:21 +0200

    Update accordion and readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A repository to hold components from [Salesforce Lightning Design System](https://react.lightningdesignsystem.com/).
 
-### Notes
+### Release Notes
 
 There were a few components left out for the first pass, ones that aren't so core to the Lightning Design System. Let's foucs on completing the tasks/components above before moving onto the ones below.
 
@@ -20,3 +20,31 @@ There were a few components left out for the first pass, ones that aren't so cor
 - Removed Visual Picker from first pass, composed of many components
 - Removed Welcome Mat for first pass (What is best practice for this? - This is a version of a modal)
 - Removed Tree from first pass, need to figure out how to make it accessible without hard coding values
+
+### Usage Notes
+
+<!-- Some components, like the Accordion, need to include certain values to make it work correctly. For example, the Accordion will need a `.json` file with data that will be rendered.
+
+The accordion can accept a `.json` file that has the following format:
+
+```
+items: [
+      {
+        id: "1",
+        summary: "Accordion Summary",
+        details: "Accordion details - A"
+      },
+      {
+        id: "2",
+        summary: "Accordion Summary",
+        details: "Accordion details - B"
+      },
+      {
+        id: "3",
+        summary: "Accordion Summary",
+        details: "Accordion details - C"
+      }
+    ]
+```
+
+An example file can be found under `extra/accordion.json`. -->
--- code/accordion.tsx
+++ code/accordion.tsx
@@ -8,27 +8,31 @@ const style: React.CSSProperties = {
   height: "100%"
 };
 
-const InnerAccordion: React.SFC = props => {
-  const [state, setState] = React.useState({
-    expandedPanels: {},
-    items: [
-      {
-        id: "1",
-        summary: "Accordion Summary",
-        details: "Accordion details - A"
-      },
-      {
-        id: "2",
-        summary: "Accordion Summary",
-        details: "Accordion details - B"
-      },
-      {
-        id: "3",
-        summary: "Accordion Summary",
-        details: "Accordion details - C"
-      }
-    ]
-  });
+const InnerAccordion = props => {
+  const [state, setState] = React.useState(
+    props.accordionItems
+      ? props.accordionItems
+      : {
+          expandedPanels: {},
+          items: [
+            {
+              id: "1",
+              summary: "Accordion Summary",
+              details: "Accordion details - A"
+            },
+            {
+              id: "2",
+              summary: "Accordion Summary",
+              details: "Accordion details - B"
+            },
+            {
+              id: "3",
+              summary: "Accordion Summary",
+              details: "Accordion details - C"
+            }
+          ]
+        }
+  );
 
   function togglePanel(event, data) {
     setState({
@@ -62,24 +66,22 @@ const InnerAccordion: React.SFC = props => {
   }
 
   return (
-    <System.IconSettings iconPath="/assets/icons">
-      <System.Accordion {...props} style={style}>
-        {state.items.map(item => {
-          return (
-            <System.AccordionPanel
-              expanded={!!state.expandedPanels[item.id]}
-              id={item.id}
-              panelContentActions={() => {}}
-              key={item.id}
-              onTogglePanel={event => togglePanel(event, item)}
-              summary={item.summary}
-            >
-              {item.details}
-            </System.AccordionPanel>
-          );
-        })}
-      </System.Accordion>
-    </System.IconSettings>
+    <System.Accordion {...props} style={style}>
+      {state.items.map(item => {
+        return (
+          <System.AccordionPanel
+            expanded={!!state.expandedPanels[item.id]}
+            id={item.id}
+            panelContentActions={() => {}}
+            key={item.id}
+            onTogglePanel={event => togglePanel(event, item)}
+            summary={item.summary}
+          >
+            {item.details}
+          </System.AccordionPanel>
+        );
+      })}
+    </System.Accordion>
   );
 };
 
@@ -90,4 +92,10 @@ Accordion.defaultProps = {
   height: 140
 };
 
-addPropertyControls(Accordion, {});
+addPropertyControls(Accordion, {
+  // accoridonItems: {
+  //   type: ControlType.File,
+  //   title: "Accordion Items",
+  //   allowedFileTypes: ["json"]
+  // }
+});
